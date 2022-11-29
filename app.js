8729fe0981d0f3a149516d42411aa3a88f5a37a9
NoZ915 <tina0915tw0915@gmail.com>
2022-11-29 10:52:08 +0800

    added: randomMessageSayHi

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ bot.on('message', function (event) {
         "嗨嗨我是路忻",
         "安安你好我叫做路忻"
       ];
-      event.reply(randomMessage[Math.floor(Math.random()) * randomMessage.length]);
+      event.reply(randomMessage[Math.floor(Math.random() * randomMessage.length)]);
     }
     randomMessageSayHi();
   } else if (event.message.text === "0019470313") {
